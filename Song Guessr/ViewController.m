ecdcc9355853796ba04e0343637a6bcc6f8ee4b1
Roger Z <the.roger.zou@gmail.com>
2014-10-18 15:20:51 -0400

    Autolayout too difficult

--- Song Guessr/ViewController.m
+++ Song Guessr/ViewController.m
@@ -31,7 +31,33 @@
         //button.titleLabel.text = self.player.nowPlayingItem.title;
         [button setTitle:self.player.nowPlayingItem.title forState:UIControlStateNormal];
     }
+    MPMediaQuery *query = [[MPMediaQuery alloc] init];
+    NSString *artist = self.player.nowPlayingItem.artist;
+    if (!artist)
+        artist = @"";
+    [query addFilterPredicate: [MPMediaPropertyPredicate
+                                predicateWithValue: artist
+                                forProperty: MPMediaItemPropertyArtist]];
+    // Sets the grouping type for the media query
+    [query setGroupingType: MPMediaGroupingAlbum];
     
+    NSArray *albums = [query collections];
+    for (MPMediaItemCollection *album in albums) {
+        MPMediaItem *representativeItem = [album representativeItem];
+        NSString *artistName =
+        [representativeItem valueForProperty: MPMediaItemPropertyArtist];
+        NSString *albumName =
+        [representativeItem valueForProperty: MPMediaItemPropertyAlbumTitle];
+        NSLog (@"%@ by %@", albumName, artistName);
+        
+        NSArray *songs = [album items];
+        for (MPMediaItem *song in songs) {
+            NSString *songTitle =
+            [song valueForProperty: MPMediaItemPropertyTitle];
+            NSLog (@"\t\t%@", songTitle);
+        }
+    }
+
     
 }
 
